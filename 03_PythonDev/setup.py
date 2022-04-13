b749c76fad1e8943ee65b50a6347d934baa01dc2
Paul <Paul@matrisk.local>
2022-04-13 14:47:24 +0200

    Moved code from interactive python example from notebook to python development folder for easier maintainance.

--- 03_PythonDev/setup.py
+++ 03_PythonDev/setup.py
@@ -5,17 +5,10 @@ install_requires = [
     'pystra@git+https://github.com/pystra/pystra',
 ]
 
-# modules to be installed with this package
-py_modules = [
-    'reliability',
-    'booktools',
-]
-
 setup(
     name = 'nrpmint',
     version = '0.1',
     author = 'Paul-Remo Wagner',
     author_email = 'wagner@matrisk.com',
-    py_modules = py_modules,
     install_requires = install_requires
 )
