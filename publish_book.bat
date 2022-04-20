9a22b515b0f9972f6f4c1bf0b0579c00d586dc48
Paul <Paul@matrisk.local>
2022-04-20 17:26:22 +0200

    Added .bat script to publish book (windows only) and updated readme to automatically write CNAME file for custom domain.

---
+++ publish_book.bat
@@ -0,0 +1,14 @@
+:: Batch script to publish the jupyter book online
+:: start anaconda promp
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+:: activate 2021_13 environment
+call conda activate 2021_13
+:: go to the base folder of the jupyter book
+cd C:\Users\Paul\Documents\Projects\2021\13_ESA\11_Code\01_JupyterBook
+:: build the jupyter book HTMLs
+jupyter-book build 02_NRPMHandbook
+:: push them to the ghp-branch with the ghp-import program
+ghp-import -n -p -f --cname=nrpmhandbook.reliability.space 02_NRPMHandbook\_build\html
+:: leave anaconda prompt (twice to get out of (base) environment as well
+call conda deactivate
+call conda deactivate
