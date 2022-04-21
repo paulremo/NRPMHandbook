1506a39f823576250c996603e5042986e303f46a
Paul <Paul@matrisk.local>
2022-04-21 13:45:41 +0200

    Updated publishing script on windows to delete _built folder before compilation.

--- publish_book.bat
+++ publish_book.bat
@@ -5,6 +5,8 @@ call C:\ProgramData\Anaconda3\Scripts\activate.bat
 call conda activate 2021_13
 :: go to the base folder of the jupyter book
 cd C:\Users\Paul\Documents\Projects\2021\13_ESA\11_Code\01_JupyterBook
+:: remove the _build folder from previous builds
+rmdir /S /Q 02_NRPMHandbook\_build
 :: build the jupyter book HTMLs
 jupyter-book build 02_NRPMHandbook
 :: push them to the ghp-branch with the ghp-import program
