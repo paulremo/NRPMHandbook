04488a0d64b0d891c7998afe2e4909147aaa98b1
csxADS <csxPFE@gmail.com>
2022-05-10 10:31:41 +0200

    advanced guide scenario 1

--- js/guide_misc.js
+++ js/guide_misc.js
@@ -14,30 +14,33 @@ class Conversation {
     }
 
     next(id) {
-        console.log("next");
-        console.log(id);
+        //console.log("next");
+        //console.log(id);
         let elt = document.getElementById(id);
         let cls = elt.className;
-        //console.log(elt)
         if (cls == "button-next") {
-            //console.log("button next");
             this.current_branch.nextStep()
         }
         else if (id == "inputComponent") {
             let cdn = elt.value;
             let res = this.current_branch.messages.get(this.current_branch.step).getResult(cdn);
             if (res != null) {
-                console.log("text : " + cdn);
+                //console.log("text : " + cdn);
                 this.current_branch.nextStep(true, res);
             }
             else {
-                console.log("nothing");
+                //console.log("nothing");
                 this.current_branch.nextStep(false);
             }
+        }
+        else {
+            console.log("else");
+            this.current_branch.nextStep(true, id);
         }
     }
 
     switchBranch(b, arg) {
+        //console.log(b);
         this.current_branch = this.branches.get(b);
         this.current_branch.init(this, arg);
     }
@@ -59,35 +62,47 @@ class Branch {
         let msg = this.messages.get(this.step);
         let tr = this.conversation.container.insertRow();
         let td = tr.insertCell(0);
+        if (msg.configurable){
+            msg.configure(this.data)
+        }
         td.appendChild(msg.htmlRep);
         setTimeout(() => { this.nextStep() }, 2000);
     }
 
     nextStep(new_branch_id = null, arg = null) {
+        console.log("next step " + arg);
         this.step += 1;
-        //console.log("next step " + this.step);
         if (this.step >= this.messages.size) {
-            //console.log("arg : " + arg)
-            this.conversation.switchBranch(this.successors.get(new_branch_id), arg);
+            console.log("end of branch : " + arg);
+            if (this.successors.get(new_branch_id) != undefined){
+                this.conversation.switchBranch(this.successors.get(new_branch_id), arg);
+            }
+            else{
+                console.log("end of conversation");
+            }
         }
         else {
-            //console.log("msg")
-            //console.log(this.messages);
+            console.log("stay on branch : " + arg);
             let next_msg = this.messages.get(this.step);
-            /*if (next_msg.configurable) {
-                next_msg.configure(this.data);
-            }*/
-            //console.log(next_msg.htmlRep)
             let tr = this.conversation.container.insertRow();
             let td = tr.insertCell(0);
-            td.appendChild(next_msg.htmlRep);
-            //console.log(this.conversation.container)
-            if (!next_msg.interactive) {
-                setTimeout(() => { this.nextStep() }, 2000);
-            }
             if (next_msg instanceof DataMessage) {
                 next_msg.configure(this.data);
             }
+            if (next_msg instanceof ConfigurableMessage) {
+                console.log("configurable message");
+                next_msg.configure(arg);
+            }
+            if (next_msg instanceof ExitMessage) {
+                console.log("Exit Message");
+                next_msg.configure(arg);
+                setTimeout(() => { this.nextStep() }, 2000);
+            }
+            td.appendChild(next_msg.htmlRep);
+            if (!next_msg.interactive) {
+                setTimeout(() => { this.nextStep() }, 2000);
+            }
+
         }
 
 
@@ -177,12 +192,41 @@ class DataMessage extends Message {
 }
 
 class ConfigurableMessage extends Message {
-    constructor(text, sender,) {
+    constructor(text, sender, dataset) {
         super(text, sender, true, false);
+        this.dataset = dataset;
     }
 
     configure(data) {
-        this.text = text + " " + data;
+        console.log("data : " + data)
+        let dt = this.dataset.get(data);
+        this.text = this.text + " " + dt;
+
+        let div = document.createElement("div");
+        div.setAttribute("class", this.sender);
+        let par = document.createElement("p");
+        par.innerHTML = this.text;
+        div.appendChild(par);
+        this.htmlRep = div;
+    }
+
+}
+
+class ExitMessage extends Message{
+    constructor(text, sender, destination) {
+        super(text,sender,true,true);
+        this.destination = destination;
+    }
+    configure(data){
+        let div = document.createElement("div");
+        div.setAttribute("class", this.sender);
+        let par = document.createElement("button");
+        par.innerHTML = this.text;
+        par.setAttribute("onclick", "window.location.href = '" + this.destination + "';");
+        par.className =  this.sender;
+        div.style.borderColor = "#383b3a";
+        div.appendChild(par);
+        this.htmlRep = div;
     }
 }
 
@@ -219,7 +263,22 @@ dataComponent = new Set([
     "RW (Reaction Wheel) GEO",
     "RW (Reaction Wheel) LEO",
     "DEPLOYMENT DEVICE Antenna, boom",
-    "DEPLOYMENT DEVICE Solar Array"
+    "DEPLOYMENT DEVICE Solar Array",
+    "RF PASSIVE",
+    "CONNECTORS",
+    "HET (Hall Effect Thruster)",
+    "VALVE LV (Latching Valve)",
+    "VALVE FCV (MONO STABLE) Flow Control Valve",
+    "VALVE PYRO VALVE",
+    "PT (Pressure Transducer)",
+    "FILTER",
+    "HEAT PIPES",
+    "THERMO SWITCH",
+    "HEATER",
+    "Pyro Actuator",
+    "Non Explosive Actuator",
+    "OPTICS",
+    "OPTRONICS",
 ])
 
 dataComponentType = new Map([
@@ -236,7 +295,22 @@ dataComponentType = new Map([
     ["RW (Reaction Wheel) GEO", "standard"],
     ["RW (Reaction Wheel) LEO", "standard"],
     ["DEPLOYMENT DEVICE Antenna, boom", "standard"],
-    ["DEPLOYMENT DEVICE Solar Array", "standard"]
+    ["DEPLOYMENT DEVICE Solar Array", "standard"],
+    ["RF PASSIVE", "non-standard"],
+    ["CONNECTORS", "non-standard"],
+    ["HET (Hall Effect Thruster)", "non-standard"],
+    ["VALVE LV (Latching Valve)", "non-standard"],
+    ["VALVE FCV (MONO STABLE) Flow Control Valve", "non-standard"],
+    ["VALVE PYRO VALVE", "non-standard"],
+    ["PT (Pressure Transducer)", "non-standard"],
+    ["FILTER", "non-standard"],
+    ["HEAT PIPES", "non-standard"],
+    ["THERMO SWITCH", "non-standard"],
+    ["HEATER", "non-standard"],
+    ["Pyro Actuator", "non-standard"],
+    ["Non Explosive Actuator", "non-standard"],
+    ["OPTICS", "non-standard"],
+    ["OPTRONICS", "non-standard"],
 ])
 
 let m1 = new Message("Welcome to the failure rate calculation guide for the miscellaneous items !", "other-message", false, false);
@@ -250,19 +324,23 @@ let m7 = new Message("I found matching components, chose the right one below", "
 
 let m8 = new DataMessage("bb", "other-message");
 
-let m9 = new ConfigurableMessage("That's a","other-message");
+let m9 = new ConfigurableMessage("This component is", "other-message", dataComponentType
+);
+
+let m10 = new ExitMessage("You can go on the handbook page", "other-message", "http://127.0.0.1:5500/02_NRPMHandbook/_build/html/miscellaneous/handbook/reliability_prediction/process_reliability_modelling.html#back_from_misc_failure_rate_processing_balise");
+let m11 = new ExitMessage("Or you can go to this page to calculate the failure rate", "other-message", "http://127.0.0.1:5500/02_NRPMHandbook/_build/html/miscellaneous/models/failure_rate_processing.html");
 
 
 let mb0 = new Map([[0, m1], [1, m2], [2, m3], [3, m4], [4, m5], [5, m6]]);
 //console.log(mb0);
 let mb1 = new Map([[0, m7], [1, m8]]);
 let mb2 = new Map([[0, new Message("b2", "other-message", false, false)]]);
-let mb3 = new Map([0, m9]);
+let mb3 = new Map([[0, m9],[1, m10],[2, m11]]);
 
-let b3 = new Branch(mb3,new Map())
-let b1 = new Branch(mb1, new Map([true, "b3"]));
+let b3 = new Branch(mb3, new Map())
+let b1 = new Branch(mb1, new Map([[true, "b3"]]));
 let b2 = new Branch(mb2, new Map());
 let b0 = new Branch(mb0, new Map([[true, "b1"], [false, "b2"]]));
 
-let b = new Map([["b0", b0], ["b1", b1], ["b2", b2]]);
+let b = new Map([["b0", b0], ["b1", b1], ["b2", b2], ["b3", b3]]);
 conv = new Conversation(document.getElementById("messagesTrack"), b);
