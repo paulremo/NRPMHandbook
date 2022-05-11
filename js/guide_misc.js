4adc33a68b043b9bedbf45108427c34939c6aaf0
csxADS <csxPFE@gmail.com>
2022-05-11 11:48:54 +0200

    changed table css

--- js/guide_misc.js
+++ js/guide_misc.js
@@ -44,8 +44,12 @@ class Conversation {
             console.log(res)
             this.current_branch.nextStep(true, res);
         }
-        else if (cls == "itemComponents"){
+        else if (cls == "itemComponents") {
             this.current_branch.nextStep(true, id);
+        }
+        else if (id == "failbuttonsearch") {
+            console.log("fail")
+            this.current_branch.nextStep(false);
         }
         else {
             //console.log("else");
@@ -54,7 +58,7 @@ class Conversation {
     }
 
     switchBranch(b, arg) {
-        //console.log(b);
+        console.log(b);
         this.current_branch = this.branches.get(b);
         this.current_branch.init(this, arg);
     }
@@ -74,13 +78,19 @@ class Branch {
         this.conversation = conversation
         this.data = arg;
         let msg = this.messages.get(this.step);
-        let tr = this.conversation.container.insertRow();
-        let td = tr.insertCell(0);
-        if (msg.configurable) {
-            msg.configure(this.data)
+        if (msg instanceof EmptyMessage) {
+            console.log("empty")
         }
-        td.appendChild(msg.htmlRep);
-        setTimeout(() => { this.nextStep() }, 500);
+        else {
+            let tr = this.conversation.container.insertRow();
+            let td = tr.insertCell(0);
+            if (msg.configurable) {
+                msg.configure(this.data)
+            }
+            td.appendChild(msg.htmlRep);
+        }
+
+        setTimeout(() => { this.nextStep() }, 1500);
     }
 
     nextStep(new_branch_id = null, arg = null) {
@@ -104,23 +114,40 @@ class Branch {
             else{
                 this.conversation.picture.setAttribute("src", "../html/pictures/astronaut1.png");
             }*/
-            let tr = this.conversation.container.insertRow();
-            let td = tr.insertCell(0);
-            if (next_msg instanceof DataMessage) {
-                next_msg.configure(this.data);
+
+            if (next_msg.extra_intercator != null) {
+                this.conversation.extra_bar.appendChild(next_msg.extra_intercator);
             }
-            if (next_msg instanceof ConfigurableMessage) {
-                //console.log("configurable message");
-                next_msg.configure(arg);
+            else{
+                let children = this.conversation.extra_bar.children;
+                for (let i = 0; i < children.length; i++) {
+                    this.conversation.extra_bar.removeChild(children[i])
+                  }
             }
-            if (next_msg instanceof ExitMessage) {
-                //console.log("Exit Message");
-                next_msg.configure(arg);
-                setTimeout(() => { this.nextStep() }, 500);
+
+            if (next_msg instanceof EmptyMessage) {
+
             }
-            td.appendChild(next_msg.htmlRep);
+            else {
+                let tr = this.conversation.container.insertRow();
+                let td = tr.insertCell(0);
+                if (next_msg instanceof DataMessage) {
+                    next_msg.configure(this.data);
+                }
+                if (next_msg instanceof ConfigurableMessage) {
+                    //console.log("configurable message");
+                    next_msg.configure(arg);
+                }
+                if (next_msg instanceof ExitMessage) {
+                    //console.log("Exit Message");
+                    next_msg.configure(arg);
+                    setTimeout(() => { this.nextStep() }, 1500);
+                }
+                td.appendChild(next_msg.htmlRep);
+            }
+
             if (!next_msg.interactive) {
-                setTimeout(() => { this.nextStep() }, 500);
+                setTimeout(() => { this.nextStep() }, 1500);
             }
 
         }
@@ -130,13 +157,14 @@ class Branch {
 }
 
 class Message {
-    constructor(text, sender, configurable, interactive, interactor = null, type = null) {
+    constructor(extra_intercator, text, sender, configurable, interactive, interactor = null, type = null) {
         this.text = text;
         this.sender = sender;
         this.type = type;
         this.configurable = configurable;
         this.interactive = interactive;
         this.interactor = interactor;
+        this.extra_intercator = extra_intercator;
 
         this.htmlRep = null;
 
@@ -163,14 +191,23 @@ class Message {
 
         this.htmlRep = div;
 
+
+
     }
 
 
 }
 
+class EmptyMessage extends Message {
+    constructor(extra_intercator, data, sender) {
+        super(extra_intercator, "", sender, false, true);
+        this.data = data;
+    }
+}
+
 class SearchMessage extends Message {
-    constructor(data, text, sender, configurable, interactive, interactor = null, type = null) {
-        super(text, sender, configurable, interactive, interactor, type);
+    constructor(extra_intercator, data, text, sender, configurable, interactive, interactor = null, type = null) {
+        super(extra_intercator, text, sender, configurable, interactive, interactor, type);
         this.data = data;
     }
 
@@ -197,8 +234,8 @@ class SearchMessage extends Message {
 }
 
 class DataMessage extends Message {
-    constructor(text, sender,  classNm = null, data = null, interactor = null, type = null) {
-        super(text, sender, true, true, interactor, type);
+    constructor(extra_intercator, text, sender, classNm = null, data = null, interactor = null, type = null) {
+        super(extra_intercator, text, sender, true, true, interactor, type);
         this.data = data;
         this.classNm = classNm;
     }
@@ -214,7 +251,7 @@ class DataMessage extends Message {
             let b = document.createElement("button");
             b.innerHTML = item;
             b.setAttribute("id", item)
-            if (this.classNm != null){
+            if (this.classNm != null) {
                 b.className = this.classNm;
             }
             b.setAttribute("onclick", "conv.next('" + item + "')")
@@ -224,8 +261,8 @@ class DataMessage extends Message {
 }
 
 class ConfigurableMessage extends Message {
-    constructor(text, sender, dataset) {
-        super(text, sender, true, false);
+    constructor(extra_intercator, text, sender, dataset) {
+        super(extra_intercator, text, sender, true, false);
         this.dataset = dataset;
     }
 
@@ -245,8 +282,8 @@ class ConfigurableMessage extends Message {
 }
 
 class ExitMessage extends Message {
-    constructor(text, sender, destination) {
-        super(text, sender, true, true);
+    constructor(extra_intercator, text, sender, destination) {
+        super(extra_intercator, text, sender, true, true);
         this.destination = destination;
     }
     configure(data) {
@@ -267,7 +304,7 @@ let conv;
 
 let buttonOK = document.createElement("button");
 buttonOK.innerHTML = "OK";
-buttonOK.setAttribute("class", "button-next");
+buttonOK.className = "button-next";
 buttonOK.setAttribute("id", "nextbutton");
 buttonOK.setAttribute("onclick", "conv.next('nextbutton')");
 
@@ -280,6 +317,12 @@ sbt.setAttribute("onclick", "conv.next('inputComponent')");
 sbt.innerHTML = "ok";
 inputComponent.appendChild(ipt);
 inputComponent.appendChild(sbt);
+
+let buttonFail = document.createElement("button");
+buttonFail.innerHTML = "Can't find component";
+buttonFail.className = "button-fail";
+buttonFail.setAttribute("id", "failbuttonsearch");
+buttonFail.setAttribute("onclick", "conv.next('failbuttonsearch')");
 
 dataComponent = new Set([
     "TWTA, Single MPM",
@@ -359,38 +402,38 @@ dataCategories = new Set([
 dataModelsByCategories = new Map([
     ["Radio Frequency", new Set(
         ["TWTA, Single MPM",
-        "TWTA, Double MPM",
-        "RF PASSIVE",]
+            "TWTA, Double MPM",
+            "RF PASSIVE",]
     )],
     ["Power", new Set(
         ["CONNECTORS",
-        "BATTERY CELL GEO",
-        "BATTERY CELL LEO",
-        "SOLAR ARRAY CELL Si",
-        "SOLAR ARRAY CELL GaAs",
-        "SADM (Solar Array drive Mechanism) GEO",
-        "SADM (Solar Array drive Mechanism) LEO",]
+            "BATTERY CELL GEO",
+            "BATTERY CELL LEO",
+            "SOLAR ARRAY CELL Si",
+            "SOLAR ARRAY CELL GaAs",
+            "SADM (Solar Array drive Mechanism) GEO",
+            "SADM (Solar Array drive Mechanism) LEO",]
     )],
     ["Propulsion", new Set(
         ["THRUSTER",
-        "TANK TUBING",
-        "HET (Hall Effect Thruster)",
-        "VALVE LV (Latching Valve)",
-        "VALVE FCV (MONO STABLE) Flow Control Valve",
-        "VALVE PYRO VALVE",
-        "PT (Pressure Transducer)",
-        "FILTER",]
+            "TANK TUBING",
+            "HET (Hall Effect Thruster)",
+            "VALVE LV (Latching Valve)",
+            "VALVE FCV (MONO STABLE) Flow Control Valve",
+            "VALVE PYRO VALVE",
+            "PT (Pressure Transducer)",
+            "FILTER",]
     )],
     ["Attitude and Orbit Control System", new Set(
         ["RW (Reaction Wheel) GEO",
-        "RW (Reaction Wheel) LEO",
-        "DEPLOYMENT DEVICE Antenna, boom",
-        "DEPLOYMENT DEVICE Solar Array",]
+            "RW (Reaction Wheel) LEO",
+            "DEPLOYMENT DEVICE Antenna, boom",
+            "DEPLOYMENT DEVICE Solar Array",]
     )],
     ["Thermal", new Set(
         ["HEAT PIPES",
-        "THERMO SWITCH",
-        "HEATER",]
+            "THERMO SWITCH",
+            "HEATER",]
     )],
     ["Pyrotechnics", new Set(
         ["Pyro Actuator",]
@@ -400,54 +443,60 @@ dataModelsByCategories = new Map([
     )],
     ["Other", new Set(
         ["OPTICS",
-        "OPTRONICS",]
+            "OPTRONICS",]
     )]
 ])
 
-let m1 = new Message("Welcome to the failure rate calculation guide for the miscellaneous items !", "other-message", false, false);
-let m2 = new Message("Before starting anything, to collect/define all technical information about the miscellaneous item in order to select the category of miscellaneous item which will be used", "other-message", false, false);
-let m3 = new Message("Is it OK ?", "other-message", false, false);
-let m4 = new Message("aa", "my-message", false, true, buttonOK, InteractType.Button);
-let m5 = new Message("Awesome ! So can you give me the name of your component ?", "other-message", false, false);
-let m6 = new SearchMessage(dataComponent, "Enter the name of the component", "my-message", false, true, inputComponent, InteractType.Input)
+let m1 = new Message(null, "Welcome to the failure rate calculation guide for the miscellaneous items !", "other-message", false, false);
+let m2 = new Message(null, "Before starting anything, to collect/define all technical information about the miscellaneous item in order to select the category of miscellaneous item which will be used", "other-message", false, false);
+let m3 = new Message(null, "Is it OK ?", "other-message", false, false);
+let m4 = new EmptyMessage(buttonOK, null, "my-message");
+let m4_1 = new Message(null, "ok", "my-message", false, false);
+let m5 = new Message(null, "Awesome ! So can you give me the name of your component ?", "other-message", false, false);
+let m6 = new SearchMessage(null, dataComponent, "Enter the name of the component", "my-message", false, true, inputComponent, InteractType.Input)
 
-let m7 = new Message("I found matching components, chose the right one below", "other-message", false, false);
+let m7 = new Message(null, "I found matching components, chose the right one below", "other-message", false, false);
 
-let m8 = new DataMessage("bb", "other-message");
+let m8 = new DataMessage(buttonFail, "bb", "other-message");
 
-let m9 = new ConfigurableMessage("This component is", "other-message", dataComponentType
+let m9 = new ConfigurableMessage(null, "This component is", "other-message", dataComponentType
 );
 
-let m10 = new ExitMessage("You can go on the handbook page", "other-message", "http://127.0.0.1:5500/02_NRPMHandbook/_build/html/miscellaneous/handbook/reliability_prediction/process_reliability_modelling.html#back_from_misc_failure_rate_processing_balise");
-let m11 = new ExitMessage("Or you can go to this page to calculate the failure rate", "other-message", "http://127.0.0.1:5500/02_NRPMHandbook/_build/html/miscellaneous/models/failure_rate_processing.html");
+let m10 = new ExitMessage(null, "You can go on the handbook page", "other-message", "http://127.0.0.1:51500/02_NRPMHandbook/_build/html/miscellaneous/handbook/reliability_prediction/process_reliability_modelling.html#back_from_misc_failure_rate_processing_balise");
+let m11 = new ExitMessage(null, "Or you can go to this page to calculate the failure rate", "other-message", "http://127.0.0.1:51500/02_NRPMHandbook/_build/html/miscellaneous/models/failure_rate_processing.html");
 
-let m12 = new Message("Maybe we use another name... To which subsystem does your component belong ?", "other-message", false, false);
-let m13 = new DataMessage("...", "other-message","categories", dataCategories);
+let m12 = new Message(null, "Maybe we use another name... To which subsystem does your component belong ?", "other-message", false, false);
+let m13 = new DataMessage(null, "...", "other-message", "categories", dataCategories);
 
-let m14 = new Message("Is this one of these components ?", "other-message", false, false);
-let m15 = new DataMessage("", "other-message", "itemComponents");
+let m14 = new Message(null, "Is this one of these components ?", "other-message", false, false);
+let m15 = new DataMessage(buttonFail, "", "other-message", "itemComponents");
 
-let m16 = new ConfigurableMessage("This component is", "other-message", dataComponentType
+let m16 = new ConfigurableMessage(null, "This component is", "other-message", dataComponentType
 );
 
-let m17 = new ExitMessage("You can go on the handbook page", "other-message", "http://127.0.0.1:5500/02_NRPMHandbook/_build/html/miscellaneous/handbook/reliability_prediction/process_reliability_modelling.html#back_from_misc_failure_rate_processing_balise");
-let m18 = new ExitMessage("Or you can go to this page to calculate the failure rate", "other-message", "http://127.0.0.1:5500/02_NRPMHandbook/_build/html/miscellaneous/models/failure_rate_processing.html");
+let m17 = new ExitMessage(null, "You can go on the handbook page", "other-message", "http://127.0.0.1:51500/02_NRPMHandbook/_build/html/miscellaneous/handbook/reliability_prediction/process_reliability_modelling.html#back_from_misc_failure_rate_processing_balise");
+let m18 = new ExitMessage(null, "Or you can go to this page to calculate the failure rate", "other-message", "http://127.0.0.1:51500/02_NRPMHandbook/_build/html/miscellaneous/models/failure_rate_processing.html");
 
+let m19 = new Message(null, "It must be a holistic component", "other-message", false, false);
+let m20 = new Message(null, "There's no standard methodology. ", "other-message", false, false);
+let m21 = new ExitMessage(null, "You can go to the forum and get help from other users", "other-message","http://127.0.0.1:51500/02_NRPMHandbook/_build/html/miscellaneous/models/failure_rate_processing.html");
 
-let mb0 = new Map([[0, m1], [1, m2], [2, m3], [3, m4], [4, m5], [5, m6]]);
+let mb0 = new Map([[0, m1], [1, m2], [2, m3], [3, m4], [4, m4_1], [5, m5], [6, m6]]);
 //console.log(mb0);
 let mb1 = new Map([[0, m7], [1, m8]]);
 let mb2 = new Map([[0, m12], [1, m13]]);
 let mb3 = new Map([[0, m9], [1, m10], [2, m11]]);
 let mb4 = new Map([[0, m14], [1, m15]])
 let mb5 = new Map([[0, m16], [1, m17], [2, m18]]);
+let mb6 = new Map([[0, m19], [1, m20], [2, m21]])
 
+let b6 = new Branch(mb6, new Map())
 let b5 = new Branch(mb5, new Map());
-let b4 = new Branch(mb4, new Map([[true, "b5"]]));
+let b4 = new Branch(mb4, new Map([[true, "b5"],[false, "b6"]]));
 let b3 = new Branch(mb3, new Map())
-let b1 = new Branch(mb1, new Map([[true, "b3"]]));
-let b2 = new Branch(mb2, new Map([[true, "b4"]]));
+let b1 = new Branch(mb1, new Map([[true, "b3"],[false, "b2"]]));
+let b2 = new Branch(mb2, new Map([[true, "b4"],[false, "b2"]]));
 let b0 = new Branch(mb0, new Map([[true, "b1"], [false, "b2"]]));
 
-let b = new Map([["b0", b0], ["b1", b1], ["b2", b2], ["b3", b3], ["b4", b4], ["b5", b5]]);
-conv = new Conversation(document.getElementById("messagesTrack"), b);
+let b = new Map([["b0", b0], ["b1", b1], ["b2", b2], ["b3", b3], ["b4", b4], ["b5", b5], ["b6", b6]]);
+conv = new Conversation(document.getElementById("messagesTrack"), b, document.getElementById("interactors"));
