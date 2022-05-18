a5b9cf795ffaaa6bff167720ae045a36fe81f0c7
csxADS <csxPFE@gmail.com>
2022-05-18 16:19:14 +0200

    fixed vanishing header syst 4 3

--- js/table_search.js
+++ js/table_search.js
@@ -7,34 +7,37 @@ function searchTable() {
     tr = table.getElementsByTagName("tr");
     console.log(tr);
     for (i = 1; i < tr.length; i++) {
-        found = false;
-        td_lst = tr[i].getElementsByTagName("td");
-        //console.log(td_lst.length);
-        j=0;
-        while (!found && j < td_lst.length){
-            td = td_lst[j];
-            console.log(td.className);
-            if (td) {
-                txtValue = td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        if (tr[i].className != "db-header") {
+            found = false;
+            td_lst = tr[i].getElementsByTagName("td");
+            //console.log(td_lst.length);
+            j = 0;
+            while (!found && j < td_lst.length) {
+                td = td_lst[j];
+                console.log(td.className);
+                if (td) {
+                    txtValue = td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        found = true;
+                    }
+                }
+                /*if (found){
+                    console.log(txtValue);
+                    console.log(found);
+                }*/
+                if (td.className == "full-span") {
                     found = true;
                 }
+                j++;
             }
-            /*if (found){
-                console.log(txtValue);
-                console.log(found);
-            }*/
-            if (td.className == "full-span"){
-                found = true;
+            if (found) {
+                tr[i].style.display = "";
             }
-            j++;
+            else {
+                tr[i].style.display = "none";
+            }
         }
-        if (found){
-            tr[i].style.display = "";
-        }
-        else{
-            tr[i].style.display = "none";
-        }
+
 
     }
 }
