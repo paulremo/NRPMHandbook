282bc2f65424bd7091d79988611ecf24b19aaaf6
Paul <Paul@matrisk.local>
2022-05-18 09:15:16 +0200

    Refactored web interface to seperate module and prepared test.

---
+++ nrpmint/interface/web.py
@@ -0,0 +1,74 @@
+import IPython
+import ipywidgets as widgets
+from ipywidgets import interactive, Layout, HBox, VBox
+
+class UI:
+    """
+    Class for web user interface interface. The interface exposes widgets (sliders/buttons/text-fields) for arguments
+    of a passed function.
+    """
+    def __init__(self, fun, n_cols=2, **kwargs):
+        """
+        Arguments:
+        fun -- function that is called with the exposed interface parameters. Arguments must be  provided as optional arguments.
+        n_cols -- number of columns the widgets should be arranged in (optional, default=2)
+
+        Optional key-value pair arguments:
+        Sequence of dictionaries or numerical values that represent the arguments to fun. The key must match
+        the required function argument. The values are
+
+        - dictionaries for each widget with keys:
+            - type -- Widget type, can be
+                - Dropdown
+                - FloatSlider
+            - description -- Display name of the widget
+            - <*> -- Additional keys that are required by the chosen widget type
+        """
+
+        # specify number of items per columns
+        num, div = len(kwargs), n_cols
+        items_per_column = [num // div + (1 if x < num % div else 0) for x in range(div)]
+
+        # initialize widgets
+        widgets_dict = {}
+        for key, value in kwargs.items():
+            # distinguish between supported widget types
+            if value['type'].casefold() == 'dropdown':
+                # remove type key and pass to widget
+                del value['type']
+                widgets_dict[key] = widgets.Dropdown(**value)
+            elif value['type'].casefold() == 'floatslider':
+                # remove type key and pass to widget
+                del value['type']
+                widgets_dict[key] = widgets.FloatSlider(**value)
+            else:
+                print(f'Provided widget {value["type"]} is not supported!')
+
+        # put inside interactive
+        ip = interactive(fun, **widgets_dict)
+
+        # setup input controls
+        input_controls = ip.children[:-1]
+
+        columns = []
+        for ii in range(n_cols):
+            if ii == 0:
+                end_idx = items_per_column[ii]
+                start_idx = 0
+            else:
+                end_idx = items_per_column[ii] + start_idx
+                start_idx = items_per_column[ii-1]
+
+            columns.append(VBox(input_controls[start_idx:end_idx]))
+
+        input_box = HBox(columns)
+
+        # setup output
+        output_stream = ip.children[-1]
+
+        # display
+        # show input fields
+        IPython.display.display(input_box)
+
+        # show output
+        IPython.display.display(output_stream)
---
+++ tests/test_interface/test_web.py
@@ -0,0 +1,31 @@
+import pytest
+
+# import module
+from nrpmint.interface.web import UI
+
+def test_web():
+    """
+    Tests whether the web interface can be created with the given options.
+    """
+
+    test_function = lambda a,b : b
+
+    # prepare slider and drop down
+    a = {
+        'type': 'dropdown',
+        'description': 'a [-]',
+        'value': 'Gumbel',
+        'options': ['LogNormal', 'Normal', 'Gumbel'],
+    }
+
+    b = {
+        'type': 'floatslider',
+        'description': 'b [$m^2$]',
+        'value': 0.5,
+        'min': 0,
+        'max': 1,
+        'step': 1e-2,
+    }
+
+    # initialize interface
+    my_interface = UI(test_function, n_cols=2, a=a, b=b)
