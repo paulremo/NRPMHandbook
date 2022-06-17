3405955508bec236353406bc8e4a4ca000a2b2e5
csxADS <csxPFE@gmail.com>
2022-06-17 10:09:46 +0200

    changed guide js 6

--- js/guide_misc.js
+++ js/guide_misc.js
@@ -555,9 +555,10 @@ window.onload = function () {
 
     let b = new Map([["b0", b0], ["b1", b1], ["b2", b2], ["b3", b3], ["b4", b4], ["b5", b5], ["b6", b6], ["b7", b7], ["b8", b8]]);
     
-    console.log("conatiner");
-    console.log(document.getElementById("messagesTrack"));
     conv = new Conversation(document.getElementById("messagesTrack"), b);
+
+    console.log("conv");
+    console.log(conv);
 
 };
 
