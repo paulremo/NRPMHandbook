00c2773a2c15211b72061521a88f6b3d1110d408
csxADS <csxPFE@gmail.com>
2022-06-20 15:19:53 +0200

    modified decision tree

--- js/decision_tree2.js
+++ js/decision_tree2.js
@@ -1,378 +1,108 @@
 window.onload = function () {
 
-    console.log("there");
+    let last_level = 1;
 
-    WIDTH = 105;
-    HEIGHT = 30;
+    let first_node;
 
-    SPACEX = 20;
-    SPACEY = 20;
+    let last_node;
 
-    class Node {
-        constructor(x, y, txt, parent) {
-            this.x = x;
-            this.y = y;
-            this.txt = txt;
-            this.parent = parent;
+    function clickLeaf(leaf){
 
-            this.color = "brown"
-
-            this.height = HEIGHT;
-            this.width = WIDTH;
-        }
-
-        drawMe(ctx) {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-            ctx.fillStyle = 'white';
-            ctx.font = '9px serif';
-            let txt_x = this.x + (this.width - ctx.measureText(this.txt).width) / 2;
-            let txt_y = this.y + 2 * HEIGHT / 3;
-            ctx.fillText(this.txt, txt_x, txt_y);
-        }
-
-        isClicked(px, py) {
-            if (px < this.x + this.width && px > this.x) {
-                if (py < this.y + this.height && py > this.y) {
-                    return true;
-                }
+        for (var l of document.getElementsByClassName("leaf-item")){
+            if (l.id.split("-")[1] <= leaf.id.split("-")[1] && l != leaf && l.innerHTML != first_node && l.innerHTML != last_node){
+                l.style.backgroundColor = "black";
             }
-            return false;
-        }
-    }
-
-    class Leaf extends Node {
-        constructor(x, y, txt, parent) {
-            super(x, y, txt, parent)
-
-            this.color = "green"
-        }
-    }
-
-    class DecisionTree {
-
-        constructor(name, levels, canvas, context) {
-
-            this.name = name;
-
-            this.levels = levels;
-
-            this.canvas = canvas;
-
-            this.context = context;
-
-            this.current_level = this.levels;
-
-            this.choices = new Map();
-
-            this.depth = 0;
-
-            this.max
-
-            this.init();
-
+            else{
+                l.style.backgroundColor = "green";
+            }
+            
+            if (l.id.split("-")[1] > leaf.id.split("-")[1]){
+                console.log(l.id)
+                l.style.display = "none";
+            }
         }
 
+        let leafName = leaf.innerHTML;
 
+        let stage = leaf.id.split("-")[1];
+        let place = leaf.id.split("-")[2];
+        leaf.style.backgroundColor = "red";
 
-        init() {
+        console.log(last_level)
+        console.log(place)
 
-            this.current_level = this.levels;
-
-            this.choices = new Map();
-
-            let n = this.current_level.size;
-
-            this.choices.set(this.depth, new Set());
-
-            this.ypar = 20;
-            let xpar;
-
-            if (n % 2 == 0) {
-                xpar = this.canvas.width / 2;
-                var i = 0;
-                if (this.current_level instanceof Map) {
-                    for (var [key, l] of this.current_level) {
-                        let xl;
-                        if (i < n / 2) {
-                            xl = xpar - WIDTH / 2 - (i - 1) * (SPACEX + WIDTH);
-                        }
-                        else {
-                            xl = xpar + WIDTH / 2 + (i - n / 2 - 1) * (SPACEX + WIDTH);
-                        }
-
-                        let yl = SPACEY;
-                        let new_leaf = new Node(xl, yl, key, null);
-                        this.choices.get(this.depth).add(new_leaf);
-
-                        i++;
-                    }
+        if (stage == "1"){
+            if (leafName == "Same parts"){
+                first_node = "Same parts";
+                last_node = "Same parts";
+                document.getElementById("leaf-2-2").innerHTML = "Fracture / Fatigue";
+                document.getElementById("leaf-2-2").style.display = "block";
+                document.getElementById("leaf-2-3").innerHTML = "Radiation degradation";
+                document.getElementById("leaf-2-3").style.display = "block";
+                document.getElementById("leaf-2-4").innerHTML = "Distortion";
+                document.getElementById("leaf-2-4").style.display = "block";
+            }
+            else{
+                first_node = "Different parts";
+                last_node = "Different parts";
+                document.getElementById("leaf-2-1").innerHTML = "Mechanical Wear";
+                document.getElementById("leaf-2-1").style.display = "block";
+                document.getElementById("leaf-2-2").innerHTML = "Fracture / Fatigue";
+                document.getElementById("leaf-2-2").style.display = "block";
+                document.getElementById("leaf-2-3").innerHTML = "Distortion";
+                document.getElementById("leaf-2-3").style.display = "block";
+            }
+        }
+        else if (stage == "2"){
+            if (leafName == "Radiation degradation"){
+                last_node = "Radiation degradation";
+                document.getElementById("leaf-3-3").innerHTML = "Fracture / Fatigue";
+                document.getElementById("leaf-3-3").style.display = "block";
+            }
+            else if (leafName == "Mechanical Wear"){
+                last_node = "Mechanical Wear";
+                document.getElementById("leaf-3-1").innerHTML = "Fracture / Fatigue";
+                document.getElementById("leaf-3-1").style.display = "block";
+            }
+            else if (leafName == "Fracture / Fatigue"){
+                last_node = "Fracture / Fatigue";
+                if (first_node == "Same parts"){
+                    document.getElementById("leaf-3-2").innerHTML = "Distortion";
+                    document.getElementById("leaf-3-2").style.display = "block";
                 }
-
+                else{
+                    document.getElementById("leaf-3-2").innerHTML = "Distortion";
+                    document.getElementById("leaf-3-2").style.display = "block";
+                }
             }
             else {
-                xpar = this.canvas.width / 2 - WIDTH / 2;
-                var i = 0;
-                if (this.current_level instanceof Map) {
-                    for (var [key, l] of this.current_level) {
-                        let xl;
-                        if (i < Math.trunc(n / 2)) {
-                            xl = xpar - i * (WIDTH + SPACEX);
-                        }
-                        else if (i > Math.trunc(n / 2)) {
-                            xl = xpar + (i - n / 2) * (SPACEX + WIDTH);
-                        }
-                        else {
-                            xl = xpar;
-                        }
-                        let yl = SPACEY;
-                        let new_leaf = new Node(xl, yl, key, null);
-                        this.choices.get(this.depth).add(new_leaf);
-
-                        i++;
-                    }
+                last_node = "Distortion";
+                if (first_node == "Same parts"){
+                    document.getElementById("leaf-3-4").innerHTML = "Mechanical wear";
+                    document.getElementById("leaf-3-4").style.display = "block";
+                    document.getElementById("leaf-3-5").innerHTML = "Fracture / Fatigue";
+                    document.getElementById("leaf-3-5").style.display = "block";
                 }
-            }
-
-            this.drawTree()
-
-        }
-
-        createLeaves(parent) {
-
-            this.current_level = this.current_level.get(parent.txt);
-
-            this.depth += 1;
-
-            let n = this.current_level.size;
-
-            this.choices.set(this.depth, new Set());
-
-            this.ypar = parent.y;
-            let xpar;
-
-            if (n % 2 == 0) {
-                xpar = parent.x + WIDTH / 2;
-                var i = 0;
-                if (this.current_level instanceof Map) {
-                    for (var [key, l] of this.current_level) {
-                        let xl;
-                        xl = (xpar + SPACEX / 2) + (i - n / 2) * (SPACEX + WIDTH);
-                        let yl = this.ypar + HEIGHT + SPACEY;
-                        let new_leaf = new Node(xl, yl, key, parent);
-                        this.choices.get(this.depth).add(new_leaf);
-
-                        i++;
-                    }
-                }
-                else {
-                    for (var item of this.current_level) {
-                        let xl;
-                        xl = (xpar + SPACEX / 2) + (i - n / 2) * (SPACEX + WIDTH);
-
-                        let yl = this.ypar + HEIGHT + SPACEY;
-                        let new_leaf = new Leaf(xl, yl, item, parent);
-                        this.choices.get(this.depth).add(new_leaf);
-
-                        i++;
-                    }
-                }
-
-            }
-            else {
-                xpar = parent.x;
-                var i = 0;
-                if (this.current_level instanceof Map) {
-                    for (var [key, l] of this.current_level) {
-                        let xl;
-                        xl = xpar + (i - Math.trunc(n / 2)) * (SPACEX + WIDTH);
-                        let yl = this.ypar + HEIGHT + SPACEY;
-                        let new_leaf = new Node(xl, yl, key, parent);
-                        this.choices.get(this.depth).add(new_leaf);
-
-                        i++;
-                    }
-                }
-                else {
-                    for (var item of this.current_level) {
-                        let xl;
-                        xl = xpar + (i - Math.trunc(n / 2)) * (SPACEX + WIDTH);
-                        let yl = this.ypar + HEIGHT + SPACEY;
-                        let new_leaf = new Leaf(xl, yl, item, parent);
-                        this.choices.get(this.depth).add(new_leaf);
-
-                        i++;
-                    }
-                }
-            }
-
-            for (var lf of this.choices.get(this.depth - 1)) {
-                if (lf != parent) {
-                    lf.color = "gray"
-                }
-                else {
-                    lf.color = "brown"
+                else{
+                    document.getElementById("leaf-3-3").innerHTML = "Mechanical wear";
+                    document.getElementById("leaf-3-3").style.display = "block";
+                    document.getElementById("leaf-3-4").innerHTML = "Fracture / Fatigue";
+                    document.getElementById("leaf-3-4").style.display = "block";
                 }
             }
         }
 
-        drawTree() {
-            for (var [key, l] of this.choices) {
-                for (var leaf of l) {
-                    leaf.drawMe(this.context)
-                }
-            }
-        }
-
-        clickLevel(event) {
-            console.log("event : " + event.x + " , " + event.y);
-            var inside = false;
-            var clicked_depth;
-            var clicked_leaf;
-            for (var [key, l] of this.choices) {
-                for (var leaf of l) {
-                    if (leaf.isClicked(event.clientX - 8, event.clientY - 8)) {
-                        inside = true
-                        clicked_depth = key;
-                        clicked_leaf = leaf;
-                    }
-                }
-            }
-
-            if (inside && !(clicked_leaf instanceof Leaf)) {
-                console.log("clicked depth : " + clicked_depth)
-                console.log("this.depth : " + clicked_depth)
-                if (clicked_depth == this.depth) {
-                    for (var lf of this.choices.get(this.depth)) {
-                        if (lf != clicked_leaf) {
-                            lf.color = "gray"
-                        }
-                    }
-                }
-                else {
-                    let new_choices = new Map();
-                    this.current_level = this.getLevel(clicked_depth);
-                    for (var k = 0; k <= parseInt(clicked_depth); k++) {
-                        new_choices.set(k, this.choices.get(k));
-                    }
-                    this.choices = new_choices;
-                    this.depth = clicked_depth;
-                    for (var lf of this.choices.get(this.depth)) {
-                        if (lf != clicked_leaf) {
-                            lf.color = "gray"
-                        }
-                    }
-                }
-                this.createLeaves(clicked_leaf);
-                this.clearCanvas();
-                this.drawTree()
-
-            }
-            return inside;
-        }
-
-        clearCanvas() {
-            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        }
-
-        getLevel(dph) {
-
-            if (dph == 0) {
-                return this.levels;
-            }
-
-            else {
-                let res = this.levels;
-
-                let parents = new Map();
-
-                for (var j = dph; j >= 0; j--) {
-                    const [first] = this.choices.get(j)
-                    let parent = first.parent;
-                    parents.set(dph - j, parent);
-                }
-
-                console.log(parents)
-
-                for (var j = 0; j < dph; j++) {
-                    res = res.get(parents.get(j).txt);
-                }
-
-                return res;
-            }
-
-        }
-
+        last_level = (parseInt(leaf.id.split("-")[1]) + 1);
+        console.log("current level : " + last_level);
     }
 
-
-
-    const levelsTest = new Map([
-
-        ["Same parts", new Map([
-
-            ["Fracture / Fatigue", new Set([
-
-                "Distortion"
-
-            ])],
-
-            ["Radiation degradation", new Set([
-
-                "Fracture / Fatigue"
-
-            ])],
-
-            ["Distortion", new Set([
-
-                "Mechanical wear",
-
-                "Fracture / Fatigue"
-
-            ])],
-
-        ])],
-
-        ["Different parts", new Map([
-
-            ["Mechanical Wear", new Set([
-
-                "Fracture / Fatigue"
-
-            ])],
-
-            ["Fracture / Fatigue", new Set([
-
-                "Distortion"
-
-            ])],
-
-            ["Distortion", new Set([
-
-                "Mechanical wear",
-
-                "Fracture / Fatigue"
-
-            ])],
-
-        ])]
-
-    ]);
-
-    console.log("here")
-
-    function run() {
-        let canvas = document.getElementById("canvasDecisionTree");
-        let context = canvas.getContext('2d');
-
-        let testTree = new DecisionTree("testTree", levelsTest, canvas, context);
-
-
-        testTree.canvas.addEventListener('click', event => {
-            testTree.clickLevel(event);
-        });
+    for (var l of document.getElementsByClassName("leaf-item")){
+        l.onclick = function () {
+            clickLeaf(this);
+        }
+        if (l.id.split("-")[1] != "1"){
+            l.style.display = "none";
+        }
     }
-
-    run();
 
 }
